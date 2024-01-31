2c926c3db9a4e9200551f6b936ca7e53a7a46865
Sfen Helaers <sfenhelaers@gmail.com>
2024-01-31 18:04:20 +0100

    2-fetch-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import pokemonLogo from "./assets/pokemon.svg";
 import "./styles/App.css";
 import "./styles/pokemon.css";
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+      const data = await response.json();
+
+      setPokemonList(data.results);
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
       <img src={pokemonLogo} className="logo" alt="Pokemon logo" />
-      <div>Start here</div>
+      <div className="pokemon-list">
+        {pokemonList.map((pokemon, index) => (
+          <pre key={index}>{JSON.stringify(pokemon)}</pre>
+        ))}
+      </div>
     </>
   );
 }
