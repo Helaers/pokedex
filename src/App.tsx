671c6d706a6d02d580c7ca61faf94fc49c99e825
Sfen Helaers <sfenhelaers@gmail.com>
2024-01-31 17:56:03 +0100

    1-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,14 @@
-import { useEffect, useState, ChangeEvent } from "react";
 import pokemonLogo from "./assets/pokemon.svg";
-import "./App.css";
-
-import Pokemon from "./components/Pokemon";
-
-type PokemonListItem = {
-	name: string;
-	url: string;
-};
+import "./styles/App.css";
+import "./styles/pokemon.css";
 
 function App() {
-	const [pokemonList, setPokemonList] = useState<PokemonListItem[]>([]);
-	// Extra
-	const [searchTerm, setSearchTerm] = useState("");
-	// End extra
-
-	useEffect(() => {
-		async function fetchData() {
-			const response = await fetch(
-				"https://pokeapi.co/api/v2/pokemon?limit=151"
-			);
-			const data = await response.json();
-
-			setPokemonList(data.results);
-		}
-		fetchData();
-	}, []);
-
-	// Extra
-	const onSearch = (event: ChangeEvent<HTMLInputElement>) => {
-		setSearchTerm(event.target.value);
-	};
-
-	const filteredPokemon = pokemonList.filter((pokemon) =>
-		pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
-	);
-
-	// End extra
-
-	return (
-		<>
-			<img src={pokemonLogo} className="logo" alt="Pokemon logo" />
-			{/* Extra */}
-			<input
-				type="text"
-				placeholder="Search Pokémon"
-				value={searchTerm}
-				onChange={onSearch}
-				className="search-input"
-			/>
-			{/* End extra */}
-			<div className="pokemon-list">
-				{/* pokemonList */}
-				{filteredPokemon.map((pokemon, index) => (
-					<Pokemon key={index} name={pokemon.name} />
-				))}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <img src={pokemonLogo} className="logo" alt="Pokemon logo" />
+      <div>Start here</div>
+    </>
+  );
 }
 
 export default App;
